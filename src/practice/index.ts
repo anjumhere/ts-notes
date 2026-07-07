d3e767fa835db75af348a2bb3b23e6b14b68bbcf
Adnan Anjum <anjumcode12@gmail.com>
2026-07-07 17:34:14 +0500

    add partial and required utility type - definition + code examples

--- src/practice/index.ts
+++ src/practice/index.ts
@@ -271,36 +271,74 @@ Duck Typing in Typescript is usually called structured typing. Ts cares about sh
 */
 
 // Object example
+//
+// type User = { name: string; age: number };
+// const person = { name: "anjum", age: 20 };
+//
+// function data(u: User) {
+//   console.log("name: ", u.name, "Age :", u.age);
+// }
+// data(person);
+//
+// type Item = { name: string; quantity: number };
+// type Address = { street: string; pin: number };
+//
+// type Order = {
+//   id: number;
+//   items: Item[];
+//   address: Address;
+// };
+//
+// const makeOrder: Order = {
+//   id: Math.floor(Math.random() * 199999999),
+//   items: [{ name: "gaming-mouse", quantity: 15000 }],
+//   address: { street: "Xth road Islamabad", pin: 3214 },
+// };
+//
+// function showOrder(order: Order) {
+//   console.log("Id: ", order.id);
+//   order.items.forEach((item) => {
+//     console.log(`Name : ${item.name} quantity: ${item.quantity}`);
+//   });
+//   console.log("Address : ", order.address.street);
+// }
+//
+// showOrder(makeOrder);
 
-type User = { name: string; age: number };
-const person = { name: "anjum", age: 20 };
+// ============================================
+// TOPIC: Partial <T> utility type
+// // ============================================
 
-function data(u: User) {
-  console.log("name: ", u.name, "Age :", u.age);
-}
-data(person);
-
-type Item = { name: string; quantity: number };
-type Address = { street: string; pin: number };
-
-type Order = {
-  id: number;
-  items: Item[];
-  address: Address;
+// You can update any value partially , means its not compulsory to add everything that you mentioned in the
+// type , even if you leave its value empty like empty object, i will not throw an error.
+type User = {
+  name: string;
+  age: number;
+  isTrue: boolean;
 };
 
-const makeOrder: Order = {
-  id: Math.floor(Math.random() * 199999999),
-  items: [{ name: "gaming-mouse", quantity: 15000 }],
-  address: { street: "Xth road Islamabad", pin: 3214 },
-};
-
-function showOrder(order: Order) {
-  console.log("Id: ", order.id);
-  order.items.forEach((item) => {
-    console.log(`Name : ${item.name} quantity: ${item.quantity}`);
-  });
-  console.log("Address : ", order.address.street);
+function updateUser(update: Partial<User>) {
+  console.log(update.name);
 }
 
-showOrder(makeOrder);
+updateUser({ name: "anjum", age: 25 });
+
+// ============================================
+// TOPIC: Required <R> utility type
+// // ============================================
+
+type Admin = {
+  name?: string;
+  age?: number;
+  isValid?: true;
+};
+
+const getAdmin = (data: Required<Admin>) => {
+  console.log(data);
+};
+
+getAdmin({
+  name: "admin",
+  age: 23,
+  isValid: true,
+});
